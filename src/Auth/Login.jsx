9fb7e6ab946ddee2b3a8ed47c235f45d274329ea
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-20 02:23:01 +0500

    comment qo'shdim

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -2,43 +2,27 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // oddiy login (faqat namuna sifatida)
-    if (username === "admin" && password === "1234") {
-      localStorage.setItem("auth", "true");
-      navigate("/");
-    } else {
-      alert("❌ Login yoki parol xato!");
-    }
-  };
 
   return (
-    <div className="login-container">
-      <h2>🔐 Login</h2>
-      <form onSubmit={handleLogin} className="login-form">
-        <input
-          type="text"
-          placeholder="Username: admin"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+       <div className="login-container">
+             <form >
+                  <input 
+                     type="text"
+                     id="email"
+                     placeholder="@nimadir kiriting"
+                     required
+                     />
 
-        <input
-          type="password"
-          placeholder="Password: 1234"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+                  <input 
+                     type="password" 
+                     id="password"
+                     placeholder="parol kiriting"
+                     />
+                     <button type="submit">Ro'yxatdan o'tish</button>
+             </form>
+       </div>   
+  )
 }
 
 export default Login;
